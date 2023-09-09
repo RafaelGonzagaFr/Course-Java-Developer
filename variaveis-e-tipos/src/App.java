208ee398f48e670476f36f1fd8c3b71cadee5619
Rafael Gonzaga <rafaelgonzag4@gmail.com>
2023-09-09 18:25:23 -0300

    1.3

---
+++ variaveis-e-tipos/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        double salarioMinimo = 1304.24;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; //cast
+
+        //string
+        String userName = "rgonzaga";
+
+        //constantes
+        final double VALOR_PI = 3.14;
+    }
+}
