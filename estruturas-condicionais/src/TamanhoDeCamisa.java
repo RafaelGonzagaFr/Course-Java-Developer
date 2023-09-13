e59942980b4024d1f748132904fb4b269a06c332
RafaelGonzaga <rafaelgonzaga@gmail.com>
2023-09-13 07:35:54 -0300

    1.7

---
+++ estruturas-condicionais/src/TamanhoDeCamisa.java
@@ -0,0 +1,18 @@
+public class TamanhoDeCamisa {
+    public static void main(String[] args) {
+        String tamanho = "P";
+
+        switch (tamanho) {
+            case "P":
+                System.out.println("Pequeno");
+                break;
+            case "M":
+                System.out.println("Médio");
+                break;
+            case "G":
+                System.out.println("Grande");
+                break;
+        }
+
+    }
+}
